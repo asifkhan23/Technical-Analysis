1848410cbdf8f6f9b0556bff68962dfd24de8ae9
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-09-16 22:29:46 -0400

    Update tech_app.py

--- tech_app.py
+++ tech_app.py
@@ -509,7 +509,7 @@ df_c = df5.copy()
 
 
 # Construct a 2 x 1 Plotly figure
-fig3 = make_subplots(rows=6, cols=1, subplot_titles=(f"{ticker.upper()} Daily Candlestick Chart", "RSI", "MACD",  "ATR", 'ADX', 'Stochastic Oscillators'))
+fig3 = make_subplots(rows=6, cols=1, specs = [[{},{}]], subplot_titles=(f"{ticker.upper()} Daily Candlestick Chart", "RSI", "MACD",  "ATR", 'ADX', 'Stochastic Oscillators'))
 
 fig3.append_trace(
     go.Candlestick(
