13dcbe4f3b6665607b452b5919a9c0cd3c41cb10
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-11-20 19:05:54 -0500

    Update tech_app.py

--- tech_app.py
+++ tech_app.py
@@ -1210,18 +1210,20 @@ legend_elements = [
 plt.legend(handles=legend_elements)
 
 
-tab1, tab2, tab3 = st.tabs(["Trend Following", "Fibonacci Retracements", 'Dow Theory'])
+# tab1, tab2, tab3 = st.tabs(["Trend Following", "Fibonacci Retracements", 'Dow Theory'])
 
-with tab1:
-    st.header("Trend Following")
-    st.plotly_chart(fig, width=800)
+# with tab1:
+#     st.header("Trend Following")
+#     st.plotly_chart(fig, width=800)
     
-with tab2:
-    st.header("Fibonacci")
-    st.write(" Retracemrnt Levels - (0=black, 0.236=red, 0.382=green, 0.5=blue, 0.618=cyan, 0.786= magenta,1 = yellow )")
-    st.write(y)
-    st.plotly_chart(fig4)
+# with tab2:
+#     st.header("Fibonacci")
+#     st.write(" Retracemrnt Levels - (0=black, 0.236=red, 0.382=green, 0.5=blue, 0.618=cyan, 0.786= magenta,1 = yellow )")
+#     st.write(y)
+#     st.plotly_chart(fig4)
 
-with tab3:
-    st.header("Dow Theory")
-    st.pyplot(fig5)
+# with tab3:
+#     st.header("Dow Theory")
+#     st.pyplot(fig5)
+
+st.plotly_chart(fig, use_container_width=True)
