4494d78bd462102b350caca1c3e757e46a2af452
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-11-20 21:19:48 -0500

    Update tech_app.py

--- tech_app.py
+++ tech_app.py
@@ -615,7 +615,7 @@ df['decycler_n'].replace(0.000000, np.nan, inplace=True)
 
 
 # Construct a 2 x 1 Plotly figure
-fig3 = make_subplots(rows=3, cols=1, subplot_titles=(f"{ticker.upper()} Daily Candlestick Chart", "RSI", "MACD",  "ATR", 'ADX', 'Stochastic Oscillators'))
+fig3 = make_subplots(rows=4, cols=1, subplot_titles=(f"{ticker.upper()} Daily Candlestick Chart", "RSI", "MACD", 'Stochastic Oscillators'))
 
 fig3.append_trace(
     go.Candlestick(
@@ -674,7 +674,7 @@ fig3.add_trace(go.Scatter(x=df.index, y=df['Downtrend'], name='Support',
                          line = dict(color='green', width=2),visible='legendonly'))
 
 fig3.append_trace(go.Scatter(x=df.index, y=df['rsi'], name='RSI',
-                         line = dict(color='green', width=4), visible='legendonly'), row = 2, col = 1)
+                         line = dict(color='green', width=4)), row = 2, col = 1)
 
 fig3.add_trace(go.Scatter(x=df.index, y=df['decycler_p'], name='Decycler Bull',
                          line = dict(color='green', width=2), visible='legendonly'))
@@ -721,13 +721,13 @@ fig3.append_trace(go.Scatter(x=df.index, y=df['ATR'], name='Average True Range',
                          line = dict(color='royalblue', width=4), visible='legendonly'), row = 2, col = 1 )
 
 fig3.append_trace(go.Scatter(x=df.index, y=df['ADX'], name='ADX',
-                         line = dict(color='red', width=4), visible='legendonly'), row = 2, col = 1)
+                         line = dict(color='red', width=4)), row = 2, col = 1)
 
 fig3.append_trace(go.Scatter(x=df.index, y=df['K'], name='Fast K',
-                         line = dict(color='blue', width=2), visible='legendonly'), row = 2, col = 1)
+                         line = dict(color='blue', width=2)), row = 4, col = 1)
 
 fig3.append_trace(go.Scatter(x=df.index, y=df['D'], name='Slow D',
-                         line = dict(color='red', width=2), visible='legendonly'), row = 2, col = 1)
+                         line = dict(color='red', width=2), visible='legendonly'), row = 4, col = 1)
 
 fig3.append_trace(go.Scatter(x=df.index, y=df['ebs_p'], name='Sinewave Bull',
                          line = dict(color='green', width=2), visible='legendonly'), row = 2, col = 1 )
